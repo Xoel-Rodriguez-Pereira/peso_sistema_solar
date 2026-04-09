858a21d1ccebfb22b61e05ef29e3ae8c7fd6b48b
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-04-09 09:54:32 +0200

    feat: Añadir lógica para utilizar argumnetos de entrada

--- src/main/java/edu/teamrocket/peso_sistema_solar/Main.java
+++ src/main/java/edu/teamrocket/peso_sistema_solar/Main.java
@@ -4,26 +4,66 @@ public class Main {
 	
 	public static void main(String[] args) {
         
-        double peso = 1.0; // kg
-        
-        for(Planeta planeta : Planeta.values()){
-            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
-         
-        System.out.println("\nTu peso en los planetas Terrestres: ");
-        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
-            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
-         
-        System.out.println("\nTu peso en los planetas Gigantes Gaseosos: ");
-        for(Planeta planeta: Planeta.getGigantesGaseosos()){
-            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
+        // Valores por defecto
+        Double masa = 1.0; // kg
+        Planeta planeta = Planeta.TIERRA;
 
-        System.out.println("\nTu peso en los planetas Gigantes Helados: ");
-        for(Planeta planeta: Planeta.getGigantesHelados()){
-            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        if (args.length == 2) {
+            try {
+                masa = Double.parseDouble(args[0]);
+                planeta = Planeta.valueOf(args[1].toUpperCase());
+
+                textoCorrecto(masa, planeta);
+            }
+
+            catch (IllegalArgumentException exception) {
+                textoExcepcion();
+            }
+
+        } else if (args.length == 1) {
+
+            try {
+                masa = Double.parseDouble(args[0]);
+                textoCorrecto(masa, planeta);
+            }
+
+            catch (IllegalArgumentException exception) {
+                textoExcepcion();
+            }
+
+        } else {
+            salidaPorDefecto(masa);
         }
+        
     }
 
+    private static void textoExcepcion() {
+        System.out.println("\nLos datos introducidos no son correctos \nLos argumentos de entrada válidos son MASA(en kg) y nombre de un PLANETA \nEn el caso de no introducirse valores saltará el caso por defecto");
+    }
+
+    private static void textoCorrecto(Double masa, Planeta planeta) {
+        System.out.printf("El peso de un objeto de %f Kg en %s es %f N%n", masa, planeta.name(), planeta.pesoSuperficie(masa));
+    }
+
+    private static void salidaPorDefecto(Double masa) {
+
+        for(Planeta planeta : Planeta.values()){
+                textoCorrecto(masa, planeta);
+            }
+            
+            System.out.println("\nPeso en los planetas Terrestres: ");
+            for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+                textoCorrecto(masa, planeta);
+            }
+            
+            System.out.println("\nPeso en los planetas Gigantes Gaseosos: ");
+            for(Planeta planeta: Planeta.getGigantesGaseosos()){
+                textoCorrecto(masa, planeta);
+            }
+
+            System.out.println("\nPeso en los planetas Gigantes Helados: ");
+            for(Planeta planeta: Planeta.getGigantesHelados()){
+                textoCorrecto(masa, planeta);
+            }
+    }
 }
